3d508782f82c58eceeffe4f9890ea90cb3b8c6d0
Sam Jones <samakj@live.co.uk>
2020-03-23 08:30:02 +0000

    Add some redux store types for use in the app

---
+++ frontend/web/src/store/types.ts
@@ -0,0 +1,27 @@
+import { Action } from 'redux';
+import { KeyedObjectType, StringKeyedObjectType } from '@samsite/types/generic-object-types';
+
+export type ActionTypeType = string;
+
+export interface ActionMethodsType {
+    [name: string]: string;
+}
+
+export interface ActionType<PayloadValueType> extends Action {
+    type: ActionTypeType;
+    payload?: KeyedObjectType<PayloadValueType>;
+}
+
+export interface ActionTypeObjectType {
+    [method: string]: ActionTypeType;
+}
+
+export interface ActionObjectType<PayloadValueType> {
+    [method: string]: (payload?: KeyedObjectType<PayloadValueType>) => ActionType<PayloadValueType>;
+}
+
+export type StoreKeyType = string;
+
+export type StoreObjectType<StoreValueType> = StringKeyedObjectType<KeyedObjectType<StoreValueType>>;
+
+export type DispatchFunctionType<PayloadValueType> = (payload?: KeyedObjectType<PayloadValueType>) => void;
