e0a922d6d018dabda642fa6c7387eca7c3dde002
Sam Jones <samakj@live.co.uk>
2020-03-29 11:58:36 +0100

    Point /professional at a ProfessionalPage component

---
+++ frontend/web/src/components/professional-page/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '@samsite/components/professional-page/style.scss';
+import { ProfessionalPagePropsType } from '@samsite/components/professional-page/types';
+
+export const ProfessionalPage: React.FunctionComponent<ProfessionalPagePropsType> = ({}) => {
+    return (
+        <main className="professional-page page-width-wrapper">
+            placeholder
+        </main>
+    );
+};
--- frontend/web/src/routing/routes.tsx
+++ frontend/web/src/routing/routes.tsx
@@ -2,6 +2,7 @@ import { lazy } from 'react';
 
 import { RouteType } from '@samsite/routing/types';
 import { PersonalPage } from '@samsite/components/personal-page';
+import { ProfessionalPage } from '@samsite/components/professional-page';
 
 export const routes: RouteType[] = [
     {
@@ -14,6 +15,12 @@ export const routes: RouteType[] = [
         path: '/personal/',
         component: PersonalPage,
         lazyComponent: lazy(() => Promise.resolve({ default: PersonalPage })),
+        exact: true,
+    },
+    {
+        path: '/professional/',
+        component: ProfessionalPage,
+        lazyComponent: lazy(() => Promise.resolve({ default: ProfessionalPage })),
         exact: true,
     },
 ];
