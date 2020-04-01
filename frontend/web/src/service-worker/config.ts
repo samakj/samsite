35ae2e40929dc1fc78a26d36d2511086148be7ae
Sam Jones <samakj@live.co.uk>
2020-04-01 14:26:47 +0100

    Make all local files all dev non-cache

--- frontend/web/src/service-worker/config.ts
+++ frontend/web/src/service-worker/config.ts
@@ -4,6 +4,5 @@ export const CACHE_FULL_NAME = `${CACHE_NAME}_${CACHE_VERSION}`;
 export const PRECACHE_FILES: string[] = [];
 export const DONT_CACHE: string[] = [];
 export const DEV_DONT_CACHE: string[] = [
-    '/static/js/',
-    '/static/css/',
+    'http://localhost:8080/',
 ];
