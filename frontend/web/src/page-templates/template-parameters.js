1c8b8ea415c3e420db371e985b7e01ba32fb001f
Sam Jones <samakj@live.co.uk>
2020-04-02 13:37:32 +0100

    Add meta tags for PWA in preperation

--- frontend/web/src/page-templates/template-parameters.js
+++ frontend/web/src/page-templates/template-parameters.js
@@ -1,4 +1,32 @@
+const themeColor = '#000000';
+const webAppCapable = 'no';
+const webAppTitle = 'Sam Jones Personal Website';
+const startUrl = '/';
+
 module.exports = {
     defaultTitle: 'Sam Jones - Personal Website',
     appMountId: 'app-root',
+    charset: 'utf-8',
+    viewport: 'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no',
+    description: 'Personal website for Sam Jones.',
+    androidMeta: {
+        themeColor,
+        androidMeta: webAppCapable,
+    },
+    iOSMeta: {
+        mobileWebAppTitle: webAppTitle,
+        mobileWebAppCapable: webAppCapable,
+        mobileWebAppStatusBarStyle: themeColor,
+    },
+    windowsMeta: {
+        navbuttonColor: themeColor,
+        tileColor: themeColor,
+        tileImage: '',
+        config: '',
+    },
+    pinnedSitesMeta: {
+        applicationName: webAppTitle,
+        toolTip: webAppTitle,
+        startUrl: startUrl,
+    },
 };
