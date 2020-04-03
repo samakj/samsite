795ec2990975a72380e5496f8516dec1a530bcca
Sam Jones <samakj@live.co.uk>
2020-04-03 10:47:24 +0100

    Perpare ProjectPage component for filtering

--- frontend/web/src/components/projects-page/index.tsx
+++ frontend/web/src/components/projects-page/index.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '@samsite/components/projects-page/style.scss';
 import { ProjectsPagePropsType } from '@samsite/components/projects-page/types';
 import { ProjectDataType, data } from '@samsite/components/projects-page/data';
 import { ProjectCard } from '@samsite/components/projects-page/project-card';
 
+const setHasAny = (set: Set<any>, testValues: any[]): boolean => {
+    for (const value of testValues) {
+        if (set.has(value)) return true
+    }
+
+    return false
+};
+
 export const ProjectsPage: React.FunctionComponent<ProjectsPagePropsType> = ({}) => {
+    const [stateFilters, updateStateFilters] = useState(new Set());
+    const [keywordFilters, updateKeywordFilters] = useState(new Set());
+    const possibleStateFilters: Set<string> = new Set();
+    const possibleKeywordFilters: Set<string> = new Set();
+    const projectCards: JSX.Element[] = [];
+
+    data.forEach(
+        (project: ProjectDataType) => {
+            let passesStateFilter: boolean = true;
+            let passesKeywordFilter: boolean = false;
+
+            if (!stateFilters.size) {
+                possibleStateFilters.add(project.state);
+            } else if (!stateFilters.has(project.state)) {
+                possibleStateFilters.add(project.state);
+                passesStateFilter = false;
+            }
+
+
+            project.keywords.forEach((keyword: string) =>  {
+                if (!keywordFilters.size) {
+                    possibleKeywordFilters.add(keyword);
+                    passesKeywordFilter = true;
+                } else if (!keywordFilters.has(keyword)) {
+                    possibleKeywordFilters.add(keyword);
+                } else {
+                    passesKeywordFilter = true;
+                }
+            });
+
+            if (passesStateFilter && passesKeywordFilter) {
+                projectCards.push(<ProjectCard project={project} key={project.name} />)
+            }
+        }
+    );
+
     return (
         <main className="projects-page page-width-wrapper">
             <div className="projects">
-            {
-                data.map((project: ProjectDataType) => <ProjectCard project={project} key={project.name} />)
-            }
+            { projectCards }
             </div>
         </main>
     );
