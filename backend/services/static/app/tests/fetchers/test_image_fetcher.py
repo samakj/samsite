fee4d31f85f4d1ee8ac0ec2b2024ab3a23db9c1a
Sam Jones <samakj@live.co.uk>
2020-04-10 17:44:23 +0100

    squish

--- backend/services/static/app/tests/fetchers/test_image_fetcher.py
+++ backend/services/static/app/tests/fetchers/test_image_fetcher.py
@@ -6,28 +6,28 @@ from fetchers.ImageFetcher import create_internal_path
 class TestCreateInternalPath:
     def test_append_store_path(self, app: SamsiteFlask) -> None:
         app.STATIC_DIR = './test'
-        path_arg = 'path'
+        path = 'path'
 
         with app.app_context():
-            assert create_internal_path(path_arg) == f"{app.STATIC_DIR}/{path_arg}"
+            assert create_internal_path(path) == f"{app.STATIC_DIR}/{path}"
 
     def test_strip_leading_slash(self, app: SamsiteFlask) -> None:
         app.STATIC_DIR = './test'
-        path_arg = 'path'
+        path = 'path'
 
         with app.app_context():
-            assert create_internal_path(f"/{path_arg}") == f"{app.STATIC_DIR}/{path_arg}"
+            assert create_internal_path(f"/{path}") == f"{app.STATIC_DIR}/{path}"
 
     def test_strip_trailing_slash(self, app: SamsiteFlask) -> None:
         app.STATIC_DIR = './test'
-        path_arg = 'path'
+        path = 'path'
 
         with app.app_context():
-            assert create_internal_path(f"{path_arg}/") == f"{app.STATIC_DIR}/{path_arg}"
+            assert create_internal_path(f"{path}/") == f"{app.STATIC_DIR}/{path}"
 
     def test_strip_dir_navigation(self, app: SamsiteFlask) -> None:
         app.STATIC_DIR = './test'
-        path_arg = 'path'
+        path = 'path'
 
         with app.app_context():
-            assert create_internal_path(f"../../../{path_arg}") == f"{app.STATIC_DIR}/{path_arg}"
+            assert create_internal_path(f"../../../{path}") == f"{app.STATIC_DIR}/{path}"
