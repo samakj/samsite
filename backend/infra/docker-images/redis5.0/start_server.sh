4401b83f732a09a22749852d8dc922ced423cb42
Sam Jones <samakj@live.co.uk>
2020-04-13 10:22:15 +0100

    Make redis password optional

--- backend/infra/docker-images/redis5.0/start_server.sh
+++ backend/infra/docker-images/redis5.0/start_server.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-redis-server --port ${VIRTUAL_PORT:-6379} #--requirepass $REDIS_PASSWORD
+if [ -z "$REDIS_PASSWORD" ]
+then
+    echo "No cache password set..."
+else
+    echo "Setting password on the redis cache..."
+    echo "requirepass $REDIS_PASSWORD" >> /redis.conf
+fi
+
+redis-server /redis.conf --port ${VIRTUAL_PORT:-6379}
