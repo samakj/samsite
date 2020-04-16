e1661c615467b0024bfe0e8a7c6207f1b8dc58c0
Sam Jones <samakj@live.co.uk>
2020-04-16 13:10:17 +0100

    Generate test database in db docker image

--- backend/infra/docker-images/postgres12.0/init/init.0.postgres.sh
+++ backend/infra/docker-images/postgres12.0/init/init.0.postgres.sh
@@ -6,3 +6,8 @@ echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
 export PGUSER="$POSTGRES_USER"
 export PGPASSWORD="$POSTGRES_PASS"
 export PGDATABASE="$POSTGRES_DBNAME"
+
+"${psql[@]}" <<-EOSQL
+	    CREATE DATABASE _test_$POSTGRES_DBNAME;
+	    GRANT ALL PRIVILEGES ON DATABASE _test_$POSTGRES_DBNAME TO $POSTGRES_USER;
+EOSQL
