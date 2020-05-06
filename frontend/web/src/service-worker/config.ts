3b63751f9d44b8a2c105f881434054b02f911ded
Sam Jones <samakj@live.co.uk>
2020-05-06 16:20:47 +0100

    Add expiry to the service worker cache

--- frontend/web/src/service-worker/config.ts
+++ frontend/web/src/service-worker/config.ts
@@ -1,6 +1,11 @@
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+
 export const CACHE_NAME = 'main';
 export const CACHE_VERSION = 'v1';
 export const CACHE_FULL_NAME = `${CACHE_NAME}_${CACHE_VERSION}`;
+export const CACHE_EXPIRY = HOUR;
 export const PRECACHE_FILES: string[] = [];
 export const DONT_CACHE: string[] = [];
 export const DEV_DONT_CACHE: string[] = [
--- frontend/web/src/service-worker/index.ts
+++ frontend/web/src/service-worker/index.ts
@@ -1,22 +1,30 @@
 import { ExtendableEvent, FetchEvent } from '@samsite/service-worker/types';
-import { CACHE_FULL_NAME, DEV_DONT_CACHE, DONT_CACHE, PRECACHE_FILES } from '@samsite/service-worker/config';
+import {
+    CACHE_EXPIRY,
+    CACHE_FULL_NAME,
+    DEV_DONT_CACHE,
+    DONT_CACHE,
+    PRECACHE_FILES
+} from '@samsite/service-worker/config';
 
 const COMBINED_DONT_CACHE = [...DONT_CACHE, ...(process.env.NODE_ENV === 'development' ? DEV_DONT_CACHE : [])];
 
 self.addEventListener('install', (event: ExtendableEvent): void => {
     console.log('Installing service worker...');
     event.waitUntil(
-        caches
-            .open(CACHE_FULL_NAME)
+        cleanCaches()
+            .then(getCurrentCache)
             .then(
                 (cache: Cache): Promise<void> => {
-                    console.log('Cache opened, adding files...');
+                    console.log(`Cache opened, adding files...`);
                     return cache.addAll(PRECACHE_FILES);
                 },
             )
-            .catch((error: Error): void => {
-                console.log('Error installing service worker: ', error);
-            }),
+            .catch(
+                (error: Error): void => {
+                    console.log('Error installing service worker: ', error);
+                }
+            )
     );
 });
 
@@ -33,6 +41,48 @@ self.addEventListener('fetch', (event: FetchEvent): void => {
     );
 });
 
+const cleanCaches = (): Promise<void> => caches
+    .keys()
+    .then(
+        (cacheNames: string[]): void => {
+            for (const cacheName of cacheNames) {
+                if (cacheName.indexOf(CACHE_FULL_NAME) == 0) {
+                    const cacheOpened = parseInt(
+                        cacheName.replace(`${CACHE_FULL_NAME}@`, ''),
+                        10,
+                    );
+
+                    if (+ new Date() - cacheOpened > CACHE_EXPIRY) {
+                        console.log(`Deleting old cache '${cacheName}'.`);
+                        caches.delete(cacheName)
+                    }
+                }
+            }
+        },
+    );
+
+const getCurrentCache = (): Promise<Cache> => caches
+    .keys()
+    .then(
+        (cacheNames: string[]): string => {
+            for (const cacheName of cacheNames) {
+                if (cacheName.indexOf(CACHE_FULL_NAME) == 0) {
+                    const cacheOpened = parseInt(
+                        cacheName.replace(`${CACHE_FULL_NAME}@`, ''),
+                        10,
+                    );
+
+                    if (+ new Date() - cacheOpened < CACHE_EXPIRY) {
+                        return cacheName
+                    }
+                }
+            }
+
+            return `${CACHE_FULL_NAME}@${+ new Date()}`
+        },
+    )
+    .then((currentCache: string): Promise<Cache> => caches.open(currentCache));
+
 const isInDontCache = (url: string): boolean => {
     for (const pattern of COMBINED_DONT_CACHE) {
         if (url.match(pattern)) return true;
@@ -47,7 +97,7 @@ const performFetchRequest = (request: Request): Promise<Response> => {
     return fetch(fetchRequestClone).then((response: Response) => {
         if (response && response.status === 200) {
             const cacheResponseClone = response.clone();
-            caches.open(CACHE_FULL_NAME).then((cache: Cache): void => {
+            getCurrentCache().then((cache: Cache): void => {
                 if (!isInDontCache(request.url)) {
                     cache.put(cacheRequestClone, cacheResponseClone);
                 }
