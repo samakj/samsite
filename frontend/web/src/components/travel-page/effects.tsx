6cbadfacc5dda30a019e53201e93cc2a7319bf1f
Sam Jones <samakj@live.co.uk>
2020-05-10 22:24:27 +0100

    Move travel page effects into own file

---
+++ frontend/web/src/components/travel-page/effects.tsx
@@ -0,0 +1,60 @@
+import { StateObjectType } from '@samsite/store/types';
+import { TravelCountryStateType, TravelLocalityStateType } from '@samsite/store/handlers/travel/types';
+import { MapMarkerType } from '@samsite/components/travel-page/map/types';
+import { CountryMarker } from '@samsite/components/travel-page/country-marker';
+import React from 'react';
+
+export const handleLocalitiesGenerator = (
+    localities: StateObjectType<TravelLocalityStateType>,
+    fetchCountries: (countryCodes: string[]) => void,
+): () => void => (): void => {
+    if (localities && Object.keys(localities).length) {
+        fetchCountries(
+            Object.values(localities).map(
+                (locality: TravelLocalityStateType): string => locality.countryCode,
+            ),
+        );
+    }
+};
+
+const BOUND_PADDING = 2;
+
+export const handleCountriesGenerator = (
+    countries: StateObjectType<TravelCountryStateType>,
+    updateBounds: (
+        bounds: [{lat: number, lng: number}, {lat: number, lng: number}],
+    ) => void,
+    updateMarkers: (markers: MapMarkerType[]) => void,
+): () => void => (): void => {
+    if (countries && Object.keys(countries).length) {
+        const countryMarkers: MapMarkerType[] = [];
+        const bounds: {
+            lat: { min: number, max: number },
+            lng: { min: number, max: number },
+        } = {
+            lat: { min: null, max: null },
+            lng: { min: null, max: null },
+        };
+
+        Object.values(countries).forEach(
+            (country: TravelCountryStateType): void => {
+                if (!bounds.lat.min || country.latitude < bounds.lat.min) bounds.lat.min = country.latitude;
+                if (!bounds.lat.max || country.latitude > bounds.lat.max) bounds.lat.max = country.latitude;
+                if (!bounds.lng.min || country.longitude < bounds.lng.min) bounds.lng.min = country.longitude;
+                if (!bounds.lng.max || country.longitude > bounds.lng.max) bounds.lng.max = country.longitude;
+
+                countryMarkers.push({
+                    latLng: [country.latitude, country.longitude],
+                    component: <CountryMarker country={country} />,
+                    key: country.countryCode,
+                });
+            },
+        );
+
+        updateBounds([
+            { lat: bounds.lat.max + BOUND_PADDING, lng: bounds.lng.min - BOUND_PADDING },
+            { lat: bounds.lat.min - BOUND_PADDING, lng: bounds.lng.max + BOUND_PADDING },
+        ]);
+        updateMarkers(countryMarkers);
+    }
+};
--- frontend/web/src/components/travel-page/index.tsx
+++ frontend/web/src/components/travel-page/index.tsx
@@ -10,11 +10,7 @@ import { fetchTravelLocalities } from '@samsite/fetchers/travel/localities';
 import { getAllTravelLocalitiesSelector } from '@samsite/selectors/travel/localities';
 import { fetchTravelCountries } from '@samsite/fetchers/travel/countries';
 import { getAllTravelCountriesSelector } from '@samsite/selectors/travel/countries';
-import { TravelCountryStateType, TravelLocalityStateType } from '@samsite/store/handlers/travel/types';
-import { MapMarkerType } from '@samsite/components/travel-page/map/types';
-import { CountryMarker } from '@samsite/components/travel-page/country-marker';
-
-const BOUND_PADDING = 2;
+import { handleCountriesGenerator, handleLocalitiesGenerator } from '@samsite/components/travel-page/effects';
 
 const DumbTravelPage: React.FunctionComponent<TravelPagePropsType> = ({
     localities,
@@ -25,60 +21,15 @@ const DumbTravelPage: React.FunctionComponent<TravelPagePropsType> = ({
     const [bounds, updateBounds] = useState(undefined);
     const [markers, updateMarkers] = useState(null);
 
-    useEffect(
-        () => {
-            onFetchTravelLocalities();
-        },
-        [],
-    );
+    useEffect(() => { onFetchTravelLocalities(); }, []);
 
     useEffect(
-        () => {
-            if (localities && Object.keys(localities).length) {
-                onFetchTravelCountries(
-                    Object.values(localities).map(
-                        (locality: TravelLocalityStateType): string => locality.countryCode,
-                    ),
-                );
-            }
-        },
+        handleLocalitiesGenerator(localities, onFetchTravelCountries),
         [localities && Object.keys(localities).length],
     );
 
     useEffect(
-        () => {
-            if (countries && Object.keys(countries).length) {
-                const countryMarkers: MapMarkerType[] = [];
-                const bounds: {
-                    lat: { min: number, max: number },
-                    lng: { min: number, max: number },
-                } = {
-                    lat: { min: null, max: null },
-                    lng: { min: null, max: null },
-                };
-
-                Object.values(countries).forEach(
-                    (country: TravelCountryStateType): void => {
-                        if (!bounds.lat.min || country.latitude < bounds.lat.min) bounds.lat.min = country.latitude;
-                        if (!bounds.lat.max || country.latitude > bounds.lat.max) bounds.lat.max = country.latitude;
-                        if (!bounds.lng.min || country.longitude < bounds.lng.min) bounds.lng.min = country.longitude;
-                        if (!bounds.lng.max || country.longitude > bounds.lng.max) bounds.lng.max = country.longitude;
-
-                        countryMarkers.push({
-                            latLng: [country.latitude, country.longitude],
-                            component: <CountryMarker country={country} />,
-                            key: country.countryCode,
-                        });
-                    },
-                );
-
-                updateBounds([
-                    { lat: bounds.lat.max + BOUND_PADDING, lng: bounds.lng.min - BOUND_PADDING },
-                    { lat: bounds.lat.min - BOUND_PADDING, lng: bounds.lng.max + BOUND_PADDING },
-                ]);
-                updateMarkers(countryMarkers);
-            }
-        },
+        handleCountriesGenerator(countries, updateBounds, updateMarkers),
         [countries && Object.keys(countries).length],
     );
 
